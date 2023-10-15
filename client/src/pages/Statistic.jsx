5c94e13d283474bf86ef0b28f17e0324fd9923de
190101101 <190101101@mail.ru>
2023-10-15 11:23:58 +0400

    1.8

--- client/src/pages/Statistic.jsx
+++ client/src/pages/Statistic.jsx
@@ -1,30 +1,29 @@
-import Header from "../components/header/Header";
-import StatisticCard from "../components/statistics/StatisticCard";
-import React, { useState, useEffect } from "react";
-import { Area, Pie } from "@ant-design/plots";
-import { Spin } from "antd";
+import Header from '../components/header/Header';
+import StatisticCard from '../components/statistics/StatisticCard';
+import React, { useState, useEffect } from 'react';
+import { Area, Pie } from '@ant-design/plots';
+import { Spin } from 'antd';
 
 const Statistic = () => {
   const [data, setData] = useState();
   const [products, setProducts] = useState([]);
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(localStorage.getItem('user'));
 
   useEffect(() => {
     asyncFetch();
   }, []);
 
   useEffect(() => {
-    const getProducts = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.REACT_APP_SERVER_URL}/server/product/all`
-        );
+    const getProducts = async() => {
+      try{
+        const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/server/product/all`)
         const data = await response.json();
         setProducts(data);
-      } catch (error) {
+
+      }catch(error){
         console.log(error);
       }
-    };
+    }
 
     getProducts();
   }, []);
@@ -34,14 +33,14 @@ const Statistic = () => {
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => {
-        console.log("fetch data failed", error);
+        console.log('fetch data failed', error);
       });
   };
 
   const config = {
     data,
-    xField: "customer",
-    yField: "subtotal",
+    xField: 'customer',
+    yField: 'subtotal',
     xAxis: {
       range: [0, 1],
     },
@@ -50,36 +49,36 @@ const Statistic = () => {
   const config2 = {
     appendPadding: 10,
     data,
-    angleField: "subtotal",
-    colorField: "customer",
+    angleField: 'subtotal',
+    colorField: 'customer',
     radius: 1,
     innerRadius: 0.6,
     label: {
-      type: "inner",
-      offset: "-50%",
-      content: "{value}",
+      type: 'inner',
+      offset: '-50%',
+      content: '{value}',
       style: {
-        textAlign: "center",
+        textAlign: 'center',
         fontSize: 14,
       },
     },
     interactions: [
       {
-        type: "element-selected",
+        type: 'element-selected',
       },
       {
-        type: "element-active",
+        type: 'element-active',
       },
     ],
     statistic: {
       title: false,
       content: {
         style: {
-          whiteSpace: "pre-wrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
+          whiteSpace: 'pre-wrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
         },
-        content: "Total",
+        content: 'Total',
       },
     },
   };
@@ -87,61 +86,59 @@ const Statistic = () => {
   const totalAmount = () => {
     const amount = data.reduce((total, item) => item.amount + total, 0);
     return `${amount.toFixed(2)}$`;
-  };
+  }
 
   return (
     <>
-      <Header />
+      <Header/>
       <h1 className="text-4xl font-bold text-center">Statistic</h1>
-        <div className="px-6 pb-20 md:pb-0">
-          <div className="statistic-section">
-            <h2 className="text-lg">
-              welcome{" "}
-              <span className="text-green-700 font-bold text-xl">
-                {user.username}
-              </span>
-            </h2>
+      {data ? 
+      <div className='px-6 pb-20 md:pb-0'>
+        <div className='statistic-section'>
+          <h2 className='text-lg'>welcome <span className='text-green-700 font-bold text-xl'>{user.username}</span></h2>
 
-            {/*  */}
-            <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4">
-              <StatisticCard
-                title={"all user"}
-                amount={data?.length}
-                img={"images/user.png"}
-              />
-
-              <StatisticCard
-                title={"total earnings"}
-                amount={totalAmount()}
-                img={"images/money.png"}
-              />
-
-              <StatisticCard
-                title={"total sales"}
-                amount={data?.length}
-                img={"images/sale.png"}
-              />
-
-              <StatisticCard
-                title={"all products"}
-                amount={products?.length}
-                img={"images/product.png"}
-              />
-            </div>
-            {/*  */}
-
-            <div className="flex justify-between gap-10 lg:flex-row flex-col items-center">
-              <div className="lg:w-1/2 lg:h-80 h-80">
-                <Area {...config} />
-              </div>
-              <div className="lg:w-1/2 lg:h-80 h-80">
-                <Pie {...config2} />
-              </div>
-            </div>
+          {/*  */}
+          <div className='statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4'>
+            <StatisticCard 
+              title={"all user"} 
+              amount={data?.length} 
+              img={"images/user.png"}
+            />
+            
+            <StatisticCard 
+              title={"total earnings"} 
+              amount={totalAmount()} 
+              img={"images/money.png"}
+            />
+            
+            <StatisticCard 
+              title={"total sales"} 
+              amount={data?.length} 
+              img={"images/sale.png"}
+            />
+            
+            <StatisticCard 
+              title={"all products"} 
+              amount={products?.length} 
+              img={"images/product.png"}
+            />
           </div>
-        </div>
-    </>
-  );
-};
+          {/*  */}
 
-export default Statistic;
+          <div className='flex justify-between gap-10 lg:flex-row flex-col items-center'>
+            <div className="lg:w-1/2 lg:h-80 h-80">
+              <Area {...config} />
+            </div>
+            <div className="lg:w-1/2 lg:h-80 h-80">
+              <Pie {...config2} />
+            </div>
+          </div> 
+
+        </div>
+      </div>
+      : <Spin size="large" className="absolute top-0 h-screen w-screen flex justify-center items-center" /> }
+    </>  
+  )
+}
+
+export default Statistic
